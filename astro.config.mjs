ce3aa8ec42734e31e7d2babcc1f36fa4f97b9629
Esteban Selvaggi <selvaggiesteban@gmail.com>
2026-06-06 12:35:20 -0300

    revert to initial working configuration that worked in past attempts

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,10 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   site: "https://selvaggiesteban.dev",
-  output: "server",
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [sitemap()],
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-    platformProxy: {
-      enabled: false
-    }
-  })
+  adapter: cloudflare()
 });

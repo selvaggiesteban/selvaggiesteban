18cfbf2d1f4249f9e7bc208a1207b8a9e6a8948a
Esteban Selvaggi <selvaggiesteban@gmail.com>
2026-06-06 11:30:46 -0300

    simplify cloudflare adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,9 +14,6 @@ export default defineConfig({
 
   integrations: [sitemap()],
   adapter: cloudflare({
-    imageService: 'cloudflare',
-    platformProxy: {
-      enabled: false
-    }
+    imageService: 'cloudflare'
   })
 });

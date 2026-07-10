a7fde70f722a0a2ecf2bf4bf2dee744d02873cd7
Esteban Selvaggi <selvaggi.esteban@gmail.com>
2026-07-10 13:33:01 -0300

    Revert "feat(ad_studio): generador de contenido visual para redes sociales"
    This reverts commit 1b68a84f3156db604d26a0c93c7ae7b3097b9c83.

--- scripts/ad_studio/generators/__init__.py
+++
@@ -1,4 +0,0 @@
-from .image_generator import generar_imagen, guardar_imagen, verificar_motores
-from .carousel_generator import generar_carrusel
-from .thumbnail_generator import generar_thumbnail
-from .video_generator import generar_video, verificar_money_printer_turbo
